2ff49fc85a22ede54c1e6f225fda05e8ddc0bd5a
kzaharenkova <k.zakharenkova@it.ecommpay.com>
2023-09-20 20:42:28 +0300

    homework 3 task 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 62662;
+        int mile = ticketPrice / 20;
+        System.out.print("Кол-во бонусных миль = ");
+        System.out.println(mile);
+    }
+}
